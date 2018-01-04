f78239e3d64446ab2dc5c020c1a503b59af92c7c
Tim Williamson <timwilliamson1337@gmail.com>
2018-01-04 15:53:32 +0000

    Added filter to jquery and lists route

--- app/app.rb
+++ app/app.rb
@@ -48,9 +48,10 @@ class MakersBnB < Sinatra::Base
     erb(:spaces)
   end
 
-  get '/spaces/list' do
+  get '/spaces/list/:filter' do
     content_type :json
-    Space.all.map {|space|
+
+    Space.all(create_filter_for(params[:filter])).map {|space|
       {
         name: space.name,
         description: space.description,
@@ -59,6 +60,16 @@ class MakersBnB < Sinatra::Base
         id: space.id
         }
     }.to_json
+  end
+
+  def create_filter_for(criterion)
+ 
+    case criterion
+      when "lowprice" then {:price.lte => 10}
+      when "midprice" then {:price.gt => 10, :price.lte => 20}
+      when "highprice" then {:price.gt => 20}
+    end
+
   end
 
   post '/spaces' do
@@ -88,6 +99,7 @@ class MakersBnB < Sinatra::Base
 
   get '/spaces/my-spaces' do
     @spaces = Space.all(user: current_user)
+    @mine = true
     erb :spaces
   end
 
