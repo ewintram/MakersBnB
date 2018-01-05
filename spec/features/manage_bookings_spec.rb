56f78410da9f9e4366c5868011124cca872da0a6
Xin Wang <wangxin00163@yahoo.co.uk>
2018-01-05 12:59:35 +0000

    Add visible bookings on myspace page

--- spec/features/manage_bookings_spec.rb
+++ spec/features/manage_bookings_spec.rb
@@ -1,8 +1,17 @@
-feature "Bookings" do 
+feature "Bookings" do
   scenario "user creates a booking for a space" do
   	sign_up
   	create_space
   	expect { book }.to change { Booking.count }.by 1
   	expect(current_path).to eq('/bookings')
   end
+
+  scenario "users can see all their bookings" do
+    sign_up
+  	create_space
+    book
+    visit ("/bookings")
+    expect(page).to have_content "Check-in date: 2018-04-12"
+  end
+
 end
