352af678efa1172a3aa5d0c1ac1573ac480adaf4
Tom Brandon <tom.j.v.brandon@gmail.com>
2018-01-05 10:59:00 +0000

    Initial css using bootstrap

--- spec/features/view_space_spec.rb
+++ spec/features/view_space_spec.rb
@@ -4,35 +4,13 @@ feature("view a space") do
   scenario("shows the information about the space") do
     sign_up
     create_space
-    visit '/spaces'
-    within("ul") do
+    save_and
+    within("#spaces") do
       click_link ("My house")
     end
     expect(current_path).to eq("/spaces/1")
     expect(page).to have_content("My house")
     expect(page).to have_content("Nice crib. It's rad")
     expect(page).to have_content("£2.00")
-  end
-
-  scenario("user can navigate back to 'my spaces'") do
-    sign_up
-    create_space
-    click_on "My spaces"
-    expect(current_path).to eq("/spaces/my-spaces")
-  end
-end
-
-feature("create new spaces") do
-  scenario("logged in user can navigate to create new space from this page") do
-    sign_up
-    click_on "Create a space"
-    expect(current_path).to eq("/spaces/my-spaces/new")
-  end
-
-  scenario("logged in user can navigate to create new space from this page") do
-    sign_up
-    visit '/spaces/my-spaces'
-    click_on "Create a space"
-    expect(current_path).to eq("/spaces/my-spaces/new")
   end
 end
