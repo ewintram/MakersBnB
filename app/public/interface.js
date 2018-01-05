a7a63003530928965efe5d43f56d17f8149eaf9d
Tim Williamson <timwilliamson1337@gmail.com>
2018-01-05 15:58:01 +0000

    made it so that you can show and hide bookings for spaces on my_spaces

--- app/public/interface.js
+++ app/public/interface.js
@@ -3,14 +3,29 @@ $( document ).ready(function(){
   	list()
   })
 
-  $("#view_bookings").click(function(){
-    $("#bookings").show();
+  for (i=0; i < $('li').length; i++){
+
+  $("#view_bookings-"+i).click(function(output){
+  	viewbookings(($(this).attr('id').split('-'))[1])
   });
 
-  $("#hide_bookings").click(function(){
-    $("#bookings").hide();
+  $("#hide_bookings-" + i).click(function(){
+    hidebookings(($(this).attr('id').split('-'))[1])
   });
+};
 });
+
+viewbookings = function(index){
+  $("#bookings-"+index).show();
+  $("#view_bookings-" + index).hide();
+  $("#hide_bookings-" + index).show();
+}
+
+hidebookings = function(index){
+  $("#bookings-" + index).hide();
+  $("#view_bookings-" + index).show();
+  $("#hide_bookings-" + index).hide();
+}
 
 list = function(filter= 'nil'){
   $.get('/spaces/list/'+ filter, function(spaces) {
