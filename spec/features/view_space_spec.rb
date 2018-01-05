239a725debe3cdfe71b036244fad0009b6d5ce28
Tom Brandon <tom.j.v.brandon@gmail.com>
2018-01-05 14:57:39 +0000

    Re committing css

--- spec/features/view_space_spec.rb
+++ spec/features/view_space_spec.rb
@@ -2,13 +2,35 @@ feature("view a space") do
   scenario("shows the information about the space") do
     sign_up
     create_space
-    save_and_open_page
-    within("div#spaces") do
+    visit '/spaces'
+    within("ul") do
       click_link ("My house")
     end
     expect(current_path).to eq("/spaces/1")
     expect(page).to have_content("My house")
     expect(page).to have_content("Nice crib. It's rad")
     expect(page).to have_content("£2.00")
+  end
+
+  scenario("user can navigate back to 'my spaces'") do
+    sign_up
+    create_space
+    click_on "My spaces"
+    expect(current_path).to eq("/spaces/my-spaces")
+  end
+end
+
+feature("create new spaces") do
+  scenario("logged in user can navigate to create new space from this page") do
+    sign_up
+    click_on "Create a space"
+    expect(current_path).to eq("/spaces/my-spaces/new")
+  end
+
+  scenario("logged in user can navigate to create new space from this page") do
+    sign_up
+    visit '/spaces/my-spaces'
+    click_on "Create a space"
+    expect(current_path).to eq("/spaces/my-spaces/new")
   end
 end
